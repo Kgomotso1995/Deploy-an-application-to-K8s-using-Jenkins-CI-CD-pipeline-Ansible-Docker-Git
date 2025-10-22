434c6e5da698647aee6bc0de586f6763486b7981
Kgomotso1995 <dungenikgomotso@gmail.com>
2025-10-22 11:00:27 +0200

    fixed ports

--- production/app3/server.js
+++ production/app3/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4003;
 
 app.get('/', (req, res) => {
   res.send('Hello from Production App 3 🚀');
